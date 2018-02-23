26d04b53cc1981004bb4318404f8f52404bb61cc
Kim Jaehyeong <imn00133@gmail.com>
2018-02-23 17:22:26 +0900

    add chater1 deeplearing from scratch practic of matplot

--- Deeplearning from scratch/chapter2/main.py
+++
@@ -1,4 +0,0 @@
-import And
-
-print("%s, %s, %s, %s" % (And.And1(0, 0), And.And1(1, 0), And.And1(0, 1), And.And1(1, 1)))
-print("%s, %s, %s, %s" % (And.And2(0, 0), And.And2(1, 0), And.And2(0, 1), And.And2(1, 1)))
