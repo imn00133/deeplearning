a73d820ce2875df96b0fe549088c475178755391
Kim Jaehyeong <imn00133@gmail.com>
2018-02-23 17:24:04 +0900

    revise chapter2 make logical_circuit,py to explan that perceptron

--- Deeplearning from scratch/chapter2/and.py
+++ Deeplearning from scratch/chapter2/and.py
@@ -1,7 +1,4 @@
-import numpy as np
-
-
-def And1(x1, x2):
+def AND(x1, x2):
     w1, w2, theta = 0.5, 0.5, 0.7
     tmp = x1*w1 + x2*w2
     if tmp <= theta:
@@ -10,12 +7,7 @@ def And1(x1, x2):
         return 1
 
 
-def And2(x1, x2):
-    x = np.array([x1, x2])
-    w = np.array([0.5, 0.5])
-    b = -0.7
-    tmp = np.sum(w*x) + b
-    if tmp <= 0:
-        return 0
-    else:
-        return 1
+AND(0, 0)
+AND(0, 1)
+AND(1, 0)
+AND(1, 1)
