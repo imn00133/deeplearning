d5c45755b84c13d1762a2d49e8724f69da83eeaa
Kim Jae Hyeong <imn00133@naver.com>
2018-07-07 05:32:26 +0000

    add Lec5 logistic classifier example Diabetes classifier

---
+++ MachineAndDeepLearningForUs/Lec5_LogisiticClassifierDiabetes.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+
+data_x = [[1, 2], [2, 3], [3, 1], [4, 3], [5, 3], [6, 2]]
+data_y = [[0], [0], [0], [1], [1], [1]]
+
+X = tf.placeholder(tf.float32, shape=[None, 2])
+Y = tf.placeholder(tf.float32, shape=[None, 1])
+W = tf.Variable(tf.random_normal([2, 1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+# Hypothesis using sigmoid: tf.div(1., 1.+ tf.exp(tf.matmul(X, W) + b)
+hypothesis = tf.sigmoid(tf.matmul(X, W) + b)
+
+# cost/Loss function
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
+
+train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
+
+# Accuracy computation
+# True if hypothesis>0.5 else False
+predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+
+# do not need tf.cast. So, I simplize this line
+accuracy = tf.reduce_mean(tf.equal(predicted, Y))
+                                  
+# Launch graph
+with tf.Session() as sess:
+    # Initialize TensorFlow variables
+    sess.run(tf.global_variables_initializer())
+    
+    for step in range(10001):
+        cost_val, _ = sess.run([cost, train], feed_dict={X: data_x, Y: data_y})
+        if step % 200 == 0:
+            print(step, cost_val)
+
+    # Accuracy report
+    h, c, a = sess.run([hypothesis, predicted, accuracy], feed_dict={X: data_x, Y: data_y})
+    print("\nHypothesis: ", h, "\nCorrect (Y): ", c, "\nAccuracy: ", a)
